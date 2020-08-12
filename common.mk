b87875397100674bd7e8f425a6d4d67a8fc67972
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    msm8998-common: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- common.mk
+++ common.mk
@@ -289,6 +289,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
+# Perfd (dummy)
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.oneplus_msm8998-libperfmgr
