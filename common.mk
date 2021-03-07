ca97dae4eaf49aac74e16c03de4ea0b113e71f1a
Pranav Vashi <neobuddy89@gmail.com>
2021-03-07 17:51:42 +0530

    msm8998-common: Switch to source built vendor.qti.hardware.perf
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: If2d7e6eed8bd15c461cb25f2b2dd5351bd44b6fb

--- common.mk
+++ common.mk
@@ -396,6 +396,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     PartsBin
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.0.vendor
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
