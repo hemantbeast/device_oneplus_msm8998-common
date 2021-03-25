7dbc12130a020b0c5f1341df8a500ea39bf8d3e8
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    msm8998-common: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- common.mk
+++ common.mk
@@ -227,7 +227,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.2-service.widevine
 
 # Fingerprint sensor
