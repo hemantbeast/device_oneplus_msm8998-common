d7538fbcc3ba0a8d1ebd0c9708c02e64f8a20841
Georg Veichtlbauer <georg@vware.at>
2021-05-14 10:48:31 +0200

    Revert "msm8998: add missing libnfc-brcm.conf"
    This reverts commit 0effd2c448e8a2c25981431ff0ef3e36fdcc4085.
    
    Change-Id: I89163725ec09446138a744285adce236b49739c0

--- common.mk
+++ common.mk
@@ -361,7 +361,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libese-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
