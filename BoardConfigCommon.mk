LineageOS/android_device_oneplus_msm8998-common

msm8998-common: Use TARGET_RECOVERY_DEVICE_DIRS
Change-Id: If78acaf8f4e9928ccecabfed85c3e371e72225a2

BoardConfigCommon.mk
@@ -162,6 +162,7 @@ TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
+TARGET_RECOVERY_DEVICE_DIRS := $(DEVICE_PATH)
 #TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
 BOARD_HAS_LARGE_FILESYSTEM := true
 

hemantbeast/device_oneplus_msm8998-common

BoardConfigCommon.mk
@@ -209,6 +209,7 @@ TARGET_SYSTEM_EXT_PROP += $(PLATFORM_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
 
 # Recovery
+TARGET_RECOVERY_DEVICE_DIRS := $(PLATFORM_PATH)
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
 #TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
 BOARD_HAS_LARGE_FILESYSTEM := true