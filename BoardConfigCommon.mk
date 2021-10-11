ef9c21d0b206bbed96c3420ce2dfa31306d23044
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-11 10:19:26 +0000

    msm8998-common: remove extra sepolicy dirs
    * They're not used
    
    Change-Id: Ia4736b31275bcedc294808e94a59997ae7fe621b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -215,8 +215,6 @@ VENDOR_SECURITY_PATCH := 2020-09-01
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
 
 # Timeservice
 BOARD_USES_QC_TIME_SERVICES := true
