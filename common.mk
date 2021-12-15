fbc40a7e08d078aed9a95ee916d44414ddecc05c
LuK1337 <priv.luk@gmail.com>
2021-12-15 17:58:52 +0100

    msm8998-common: RIP com.dsi.ant.antradio_library
    No longer shipped by default with lineage-19.
    
    Change-Id: Iae740ef18505b7f2bb01fe54d45810e72fd521e9
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- common.mk
+++ common.mk
@@ -97,6 +97,11 @@ PRODUCT_COPY_FILES += \
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant@1.0.vendor
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
@@ -146,15 +151,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
-
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant.antradio_library \
-    com.dsi.ant@1.0.vendor
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
