8ea68b7ea32d1ba0abb2ca914c3f5bb939622d60
Namkhai B <me@forkbomb9.ch>
2022-04-13 12:42:18 -0500

    msm8998-common: Import DeviceSettings from yaap
    - Imported from
      https://github.com/yaap-op5t/yaap_device_oneplus_msm8998-common/commit/23bd1ed82d08941d840dede378fe2694ab0ded3b
    
    Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>
    Co-Authored-By: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-Authored-By: Anirudh Gupta <anirudhgupta109@aosip.dev>
    Co-Authored-By: David Sn <divad.nnamtdeis@gmail.com>
    Co-Authored-By: NurKeinNeid <mralexman3000@gmail.com>
    Co-Authored-By: Omkar Chandorkar <forumomkar@gmail.com>
    Co-Authored-By: enesykaya <enesykaya@gmail.com>
    Co-Authored-By: Adhitya Mohan <me@adhityamohan.in>
    Co-Authored-By: Daniel Solomon <solomondaniel@google.com>
    Co-Authored-By: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-Authored-By: LuK1337 <priv.luk@gmail.com>
    Co-Authored-By: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-Authored-By: Murtaza@Piplod <murtaza1926@gmail.com>
    Co-Authored-By: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-Authored-By: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-Authored-By: SKULSHADY <anushekprasal@gmail.com>
    Co-Authored-By: ashwin4rc <ashwin2001achu@gmail.com>
    Co-Authored-By: itsjoeoui <itsjoeoui@pixelexperience.org>
    Co-Authored-By: maitreya29 <maitreyapatni30@gmail.com>
    Co-Authored-By: maxwen <max.weninger@gmail.com>
    Change-Id: I85f034aec56a382d35af97319fde2a2360d9716b
    Signed-off-by: Namkhai B <me@forkbomb9.ch>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,18 +25,18 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.rice.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.rice.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.displaymodes.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.displaymodes.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
