6ee339ac1db6f9f1fe09ea3e8d05156ee684f396
Hemant Sharma <hemantbeast@gmail.com>
2022-05-15 13:57:09 +0530

    Update qcril
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- common.mk
+++ common.mk
@@ -438,10 +438,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4.vendor \
-    android.hardware.radio.config@1.2vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor \
     librmnetctl \
     libxml2 \
     libprotobuf-cpp-full
