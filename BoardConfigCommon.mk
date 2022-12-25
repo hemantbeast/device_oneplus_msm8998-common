e88d4d2495d1748b243cf4978a62a234a6f0d843
Nauval Rizky <enuma.alrizky@gmail.com>
2022-12-25 13:50:50 +1100

    msm8998-common: power: Add custom boost extension for scroll handling
    Currently, we're not using CAF's BoostFramework due to having a pure
    AOSP based source, thus qcom perf hinting are not correctly applied.
    This extension will handle the vertical fling through device-specific
    boost override, utilizing existing libqti-perfd hinting.
    So any further configuration should be happens inside vertical scroll
    config inside perfboostsconfig.xml
    
    Reference: vendor/qcom/opensource/power/power-6150.c w/ reduced
    max interactive timeout.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,5 +142,8 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+# Power
+TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/configs/power/boost-ext.cpp
+
 # inherit from the proprietary version
 include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
