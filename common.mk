f34b523aa8f2a19ebba28e04a035c37a7b681455
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-03-31 22:29:01 +0200

    msm8998-common: switch from time_daemon to TimeKeep
    TimeKeep is provided by Sony and it is open source. It also works with
    kernel 4.14, so just use it instead of the closed source time_daemon.
    
    Change-Id: Id169d62bfaed6b584aa935250d817655907b9e40
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- common.mk
+++ common.mk
@@ -474,3 +474,8 @@ PRODUCT_COPY_FILES += \
 # RmNet xml config file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml
+
+# OSS Time service
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
