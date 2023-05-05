5ae63210608bc5f419905af0725155eb1f7a070c
Namkhai B <me@forkbomb9.ch>
2023-05-05 10:55:40 -0600

    msm8998-common: Add qcom-caf/ to QCOM_SOONG_NAMESPACE

--- common.mk
+++ common.mk
@@ -404,6 +404,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/oneplus
+QCOM_SOONG_NAMESPACE := $(LOCAL_PATH)/qcom-caf
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
