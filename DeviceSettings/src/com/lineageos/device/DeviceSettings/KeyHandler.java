ac899cd56102c8943df5939363bcff0cafde7577
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-06-13 17:28:07 -0400

    msm8998-common: DeviceSettings: Cleanup if service gets destroyed
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- DeviceSettings/src/com/lineageos/device/DeviceSettings/KeyHandler.java
+++ DeviceSettings/src/com/lineageos/device/DeviceSettings/KeyHandler.java
@@ -126,6 +126,12 @@ public class KeyHandler extends Service {
     }
 
     @Override
+    public void onDestroy() {
+        this.unregisterReceiver(mBroadcastReceiver);
+        mAlertSliderEventObserver.stopObserving();
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
