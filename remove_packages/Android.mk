aa2e6e8afeb646d66f62ce5b23125c39a5d92ed9
Hemant Sharma <hemantbeast@gmail.com>
2024-07-13 13:24:47 +0530

    msm8998-common: Update remove packages list
     * Remove apps which can be download manually
    
    Change-Id: I92c925a693f6f151ae768a7cc58c6fc598530ae7
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,21 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := MusicFX Music AudioFX Eleven Jelly 
+LOCAL_OVERRIDES_PACKAGES := \
+    Chrome \
+    Chrome-Stub \
+    Drive \
+    Maps \
+    Photos \
+    PixelWallpapers2022 \
+    PrebuiltGmail \
+    SafetyHubPrebuilt \
+    Videos \
+    YouTube \
+    Gallery2 \
+    Eleven \
+    AudioFX
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
