4c411b98de1220b13bc8a8bd817f6b2cc98cd5e0
Hemant Sharma <hemantbeast@gmail.com>
2024-07-13 13:26:13 +0530

    msm8998-common: Include ViPER4Android if available
    Change-Id: Id4b4a94389854438760462e19207c3808b1c87d8
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- common.mk
+++ common.mk
@@ -123,6 +123,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Call the ViperFX Config
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4:64 \
